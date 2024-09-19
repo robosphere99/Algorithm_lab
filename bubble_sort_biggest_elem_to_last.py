884df8c31aa9aca8ddc92df3cc9e8b5ed2abfb14
Robosphere <anilalok99mahalpar@gmail.com>
2024-09-19 09:26:09 +0530

    biggest elem to last

--- bubble_sort_biggest_elem_to_last.py
+++ bubble_sort_biggest_elem_to_last.py
@@ -3,6 +3,6 @@ def bubble_sort(arr) :
         for j in range(len(arr)-i-1):
             if arr[j]>arr[j+1]:
                 arr[j],arr[j+1] = arr[j+1],arr[j]
-arr = [3,5,2,7,1,9,12,20,0,-5]
+arr = [3,5,2,7,1,9,12,20,2,0,-5]
 bubble_sort(arr)
 print(arr)
